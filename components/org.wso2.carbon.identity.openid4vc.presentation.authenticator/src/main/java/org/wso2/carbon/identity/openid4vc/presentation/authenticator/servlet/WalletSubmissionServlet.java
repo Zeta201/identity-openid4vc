7ebbe775861e47186ee7f6bde2f09fecf7bc94e8
Dinitha Wijewardhana <dini2040gh@gmail.com>
2026-09-04 11:17:17 +0530

    Fix checkstyle: remove unused ServletException import in WalletSubmissionServlet

--- components/org.wso2.carbon.identity.openid4vc.presentation.authenticator/src/main/java/org/wso2/carbon/identity/openid4vc/presentation/authenticator/servlet/WalletSubmissionServlet.java
+++ components/org.wso2.carbon.identity.openid4vc.presentation.authenticator/src/main/java/org/wso2/carbon/identity/openid4vc/presentation/authenticator/servlet/WalletSubmissionServlet.java
@@ -47,7 +47,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.Servlet;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
