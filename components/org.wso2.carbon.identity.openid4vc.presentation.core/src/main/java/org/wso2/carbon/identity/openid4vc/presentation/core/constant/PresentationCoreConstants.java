d234c869521b371f0b203b0486af2355e26f3423
Dinitha Wijewardhana <dini2040gh@gmail.com>
2026-09-18 15:48:35 +0530

    Rename request and response uri paths

--- components/org.wso2.carbon.identity.openid4vc.presentation.core/src/main/java/org/wso2/carbon/identity/openid4vc/presentation/core/constant/PresentationCoreConstants.java
+++ components/org.wso2.carbon.identity.openid4vc.presentation.core/src/main/java/org/wso2/carbon/identity/openid4vc/presentation/core/constant/PresentationCoreConstants.java
@@ -27,9 +27,8 @@ public final class PresentationCoreConstants {
 
     public static final String RESPONSE_MODE_DIRECT_POST_JWT = "direct_post.jwt";
 
-    public static final String REQUEST_URI_ENDPOINT = "/openid4vp/v1/presentation/";
-    public static final String RESPONSE_URI_ENDPOINT = "/openid4vp/v1/presentation";
-
+    public static final String CONTEXT_OID4VP_REQUESTS = "/oid4vp/requests";
+    public static final String CONTEXT_OID4VP_RESPONSES = "/oid4vp/responses";
 
     public static final String CLAIM_CLIENT_METADATA = "client_metadata";
     public static final String METADATA_CLIENT_NAME = "client_name";
